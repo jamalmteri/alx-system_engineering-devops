8876cac1ef0bd750f6252a2c5dc1ab69ef4cb844
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-04-05 14:53:59 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# install puppet-lint
-package{ 'puppet-lint':
-  ensure => '2.5.0',
-  provider => 'gem',
+# Install Puppet-Lint package
+class { 'puppet_lint':
+  ensure => present,
+}
+# Define package resource type
+define mypackage (
+  $ensure   = 'installed',
+  $provider = 'yum',
+) {
+  package { $name:
+    ensure   => $ensure,
+    provider => $provider,
+  }
 }
